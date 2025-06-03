a05c41d3e812e43ac149a58817d0704b3e330964
LotorSVK <edwinkatona@gmail.com>
2025-06-03 20:39:07 +0200

    Git comit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        //Random nahCislo = new Random();
+
+        RodinnyDum dumNovaku = new RodinnyDum();
+        dumNovaku.pocetPoschodi = 4;
+        dumNovaku.rozloha = 457f;
+        dumNovaku.material = "mramor";
+
+        RodinnyDum dumZajacov = new RodinnyDum();
+        dumZajacov.pocetPoschodi = 2;
+        dumZajacov.rozloha = 562f;
+
+        String nejakyText ="neco";
+
+
     }
 }
---
+++ src/RodinnyDum.java
@@ -0,0 +1,11 @@
+public class RodinnyDum {
+    int pocetPoschodi;
+    String material;
+    float rozloha;
+    int pocetOken
+
+    public void  vypocetCenyDomu(){
+        System.out.println(rozloha * 8000);
+    }
+
+}
