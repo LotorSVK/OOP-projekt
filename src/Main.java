ac998d0eea0a16f79b452ebbe07e60f2f588d754
LotorSVK <edwinkatona@gmail.com>
2025-06-03 20:57:13 +0200

    Git comit

--- src/Main.java
+++ src/Main.java
@@ -3,19 +3,22 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args) {
 
-        //Random nahCislo = new Random();
+        Random nahCislo = new Random();
+        nahCislo.nextInt();
 
         RodinnyDum dumNovaku = new RodinnyDum();
         dumNovaku.pocetPoschodi = 4;
         dumNovaku.rozloha = 457f;
         dumNovaku.material = "mramor";
-        dumNovaku.vypocetCenyDomu();
-
+        dumNovaku.vypocetCenyDomu(300, "Novákovci ");
 
         RodinnyDum dumZajacov = new RodinnyDum();
         dumZajacov.pocetPoschodi = 3;
         dumZajacov.rozloha = 6468f;
-        dumZajacov.vypocetCenyDomu();
+        dumZajacov.vypocetCenyDomu(250, "Zajicovy ");
+
+
+
 
     }
 }
--- src/RodinnyDum.java
+++ src/RodinnyDum.java
@@ -4,8 +4,8 @@ public class RodinnyDum {
     float rozloha;
     int pocetOken;
 
-    public void vypocetCenyDomu(){
-        System.out.println(rozloha * 8000);
+    public void vypocetCenyDomu(int cenaZaMeter, String menoRodiny){
+        System.out.println(menoRodiny + (rozloha * cenaZaMeter * pocetPoschodi));
     }
 
 }
